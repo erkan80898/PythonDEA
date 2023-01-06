e410454d6cc93e3a63e427ba8c084efcac14cab9
erkan808987@gmail.com <32151823+erkan80898@users.noreply.github.com>
2023-01-06 11:54:23 -0500

    Added parity drop for the key transformation from 64 bit -> 56

--- des.py
+++ des.py
@@ -1,5 +1,4 @@
-
-initial_perm = [58, 50, 42, 34, 26, 18, 10, 2,
+INITIAL_PERM = [58, 50, 42, 34, 26, 18, 10, 2,
                 60, 52, 44, 36, 28, 20, 12, 4,
                 62, 54, 46, 38, 30, 22, 14, 6,
                 64, 56, 48, 40, 32, 24, 16, 8,
@@ -8,7 +7,7 @@ initial_perm = [58, 50, 42, 34, 26, 18, 10, 2,
                 61, 53, 45, 37, 29, 21, 13, 5,
                 63, 55, 47, 39, 31, 23, 15, 7]
 
-final_perm = [40, 8, 48, 16, 56, 24, 64, 32,
+FINAL_PERM = [40, 8, 48, 16, 56, 24, 64, 32,
               39, 7, 47, 15, 55, 23, 63, 31,
               38, 6, 46, 14, 54, 22, 62, 30,
               37, 5, 45, 13, 53, 21, 61, 29,
@@ -17,23 +16,31 @@ final_perm = [40, 8, 48, 16, 56, 24, 64, 32,
               34, 2, 42, 10, 50, 18, 58, 26,
               33, 1, 41, 9, 49, 17, 57, 25]
 
-exp_perm = [32, 1, 2, 3, 4, 5, 4, 5,
-         6, 7, 8, 9, 8, 9, 10, 11,
-         12, 13, 12, 13, 14, 15, 16, 17,
-         16, 17, 18, 19, 20, 21, 20, 21,
-         22, 23, 24, 25, 24, 25, 26, 27,
-         28, 29, 28, 29, 30, 31, 32, 1]
+EXP_PERM = [32, 1, 2, 3, 4, 5, 4, 5,
+            6, 7, 8, 9, 8, 9, 10, 11,
+            12, 13, 12, 13, 14, 15, 16, 17,
+            16, 17, 18, 19, 20, 21, 20, 21,
+            22, 23, 24, 25, 24, 25, 26, 27,
+            28, 29, 28, 29, 30, 31, 32, 1]
 
-func_perm = [16,  7, 20, 21,
-       29, 12, 28, 17,
-       1, 15, 23, 26,
-       5, 18, 31, 10,
-       2,  8, 24, 14,
-       32, 27,  3,  9,
-       19, 13, 30,  6,
-       22, 11,  4, 25]
+FUNC_PERM = [16, 7, 20, 21,
+             29, 12, 28, 17,
+             1, 15, 23, 26,
+             5, 18, 31, 10,
+             2, 8, 24, 14,
+             32, 27, 3, 9,
+             19, 13, 30, 6,
+             22, 11, 4, 25]
 
-sbox = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+PARITY_DROP = [57, 49, 41, 33, 25, 17, 9, 1,
+               58, 50, 42, 34, 26, 18, 10, 2,
+               59, 51, 43, 35, 27, 19, 11, 3,
+               60, 52, 44, 36, 63, 55, 47, 39,
+               31, 23, 15, 7, 62, 54, 46, 38,
+               30, 22, 14, 6, 61, 53, 45, 37,
+               29, 21, 13, 5, 28, 20, 12, 4]
+
+SBOX = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
          [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
          [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
          [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
@@ -75,21 +82,21 @@ sbox = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
 
 test = 0xA51FF2A7C67BA384
 
+
 def set_bit(bits, pos, on):
     if on:
         return bits | (1 << pos - 1)
     else:
         return bits & ~(1 << pos - 1)
 
+
 def get_bit(bits, pos):
     return (bits >> (pos - 1)) & 1
 
-def prem(block, type=True):
+
+def prem(block, table):
     result = 0
     count = 1
-    table = initial_perm
-    if not type:
-        table = final_perm
 
     for idx in table:
         if get_bit(block, idx):
@@ -99,14 +106,16 @@ def prem(block, type=True):
         count += 1
     return result
 
+
 def partition(block):
     mask = 0xFFFFFFFF
     return block >> 32, block & mask
 
+
 def expansion(block):
     result = 0
     count = 1
-    for idx in exp_perm:
+    for idx in EXP_PERM:
         if get_bit(block, idx):
             result = set_bit(result, count, True)
         else:
@@ -114,8 +123,10 @@ def expansion(block):
         count += 1
     return result
 
+
 def apply_round_key(blocks, key):
     return map(lambda x: x ^ key, blocks)
+
 
 def apply_sbox(blocks):
     result = []
@@ -126,11 +137,11 @@ def apply_sbox(blocks):
     for block in blocks:
         row = ((block & mask_MSB) >> 4 | (block & mask_LSB))
         col = (block & mask_inner) >> 1
-        result.append(sbox[count][row][col])
+        result.append(SBOX[count][row][col])
         count += 1
     return result
 
-post_IP = prem(test)
+post_IP = prem(test, INITIAL_PERM)
 (left, right) = partition(post_IP)
 y = expansion(left) >> 42
 z = apply_sbox([y])
